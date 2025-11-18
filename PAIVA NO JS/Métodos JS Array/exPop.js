9da0babf7759699c9a8702900d192e90201f12a9
Vinícius Paiva <viniciuspaiva0011@gmail.com>
2025-11-18 16:29:40 -0300

    PAIVA JS

--- PAIVA NO JS/Métodos JS Array/exPop.js
+++ PAIVA NO JS/Métodos JS Array/exPop.js
@@ -2,5 +2,6 @@ let lanches = ["X-Burguer", "Pizza", "Sushi", "Açai"]
 
 //REMOVE O ULTIMO ELEMENTO
 let removido = lanches.pop()
+
 console.log(removido) // "Açaí"
-console.log(lanches) // ["X-Burguer", "Pizza", "Sushi", "Açai"]
+console.log(lanches) // [ "X-Burguer","Pizza","Sushi","Açai" ]
