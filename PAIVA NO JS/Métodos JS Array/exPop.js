a42f48d1d687a8377cdaf327e95e2bcc23bd005e
Vinícius Paiva <viniciuspaiva0011@gmail.com>
2025-11-18 16:12:23 -0300

    EXEMPLO POP

---
+++ PAIVA NO JS/Métodos JS Array/exPop.js
@@ -0,0 +1,6 @@
+let lanches = ["X-Burguer", "Pizza", "Sushi", "Açai"]
+
+//REMOVE O ULTIMO ELEMENTO
+let removido = lanches.pop()
+console.log(removido) // "Açaí"
+console.log(lanches) // ["X-Burguer", "Pizza", "Sushi", "Açai"]
