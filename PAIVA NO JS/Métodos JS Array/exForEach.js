378607a077606f19550a92abab19cc0fd0514658
Vinícius Paiva <viniciuspaiva0011@gmail.com>
2025-11-18 16:53:51 -0300

    PAIVA NO JS

--- PAIVA NO JS/Métodos JS Array/exForEach.js
+++ PAIVA NO JS/Métodos JS Array/exForEach.js
@@ -1,5 +1,13 @@
+
+
 let games = ["Minecraft", "Fortnite", "Roblox"]
 
+// Mostrar cada game com um emoji
 games.forEach((game, index) => {
     console.log(`${index + 1}. ${game} `)
 })
+
+// Resultado:
+// 1. Minecraft
+// 2. Fortnite
+// 3. Roblox
