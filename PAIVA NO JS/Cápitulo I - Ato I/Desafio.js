aa28607862fc193585626336946579c335594dd7
Vinícius Paiva <viniciuspaiva0011@gmail.com>
2025-11-19 14:12:49 -0300

    DESAFIO EM JS

---
+++ PAIVA NO JS/Cápitulo I - Ato I/Desafio.js
@@ -0,0 +1,10 @@
+let playlist = [" Dua Lipa - Dance The Night", "The Wekend - Blinding Lights"]
+
+playlist.push("Brain Stew", "Crazy Train")
+console.log(jogos)
+
+let removido = playlist.pop()
+
+playlist.forEach((playlist, index) => {
+    console.log(`${index + 1}. ${playlist} `)
+})
