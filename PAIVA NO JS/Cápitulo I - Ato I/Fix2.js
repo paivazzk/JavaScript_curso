a274d595d890af516b34522161748ef91646bf24
Vinícius Paiva <viniciuspaiva0011@gmail.com>
2025-11-19 15:01:15 -0300

    PAIVA NO JS

--- PAIVA NO JS/Cápitulo I - Ato I/Fix2.js
+++ PAIVA NO JS/Cápitulo I - Ato I/Fix2.js
@@ -3,7 +3,7 @@ let Nomes = ["Paiva", "Leococo", "Acin"]
 
 Nomes.push("Lukas", "Kauan")
 
-Nomes.pop()
+let remover = Nomes.pop()
 
 console.log(Nomes)
 console.log(remover)
---
+++ PAIVA NO JS/Cápitulo I - Ato I/Fix3.js
@@ -0,0 +1,6 @@
+let Number = [1,2,3,4,5]
+
+let dobrados = Number.map((Number) => {
+    return Number * 2
+})
+console.log(dobrados)
