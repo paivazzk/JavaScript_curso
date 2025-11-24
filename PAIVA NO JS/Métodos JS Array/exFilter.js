3f7d445c52359c7f9c7cc879f82b9e6cb22fa390
Vinícius Paiva <viniciuspaiva0011@gmail.com>
2025-11-24 16:41:17 -0300

    PAIVA  O JS

--- PAIVA NO JS/Métodos JS Array/exFilter.js
+++ PAIVA NO JS/Métodos JS Array/exFilter.js
@@ -1,13 +1,15 @@
-// Filtrar notas acima da média usando Arrow Function 
-let notas = [4.5, 8.0, 6.5, 9.0, 3.0]
+const amigos = [
+  {nome: "Paiva", cidade : "Tokyo", idade: "16"},
+  {nome: "Leopoldino", cidade: "Pindamonhangaba", idade: "16"},
+  {nome: "Wallace", cidade: "Macuco", idade: "23"}
+];
+ 
+const resultado = amigos.filter(u => u.cidade === "Tokyo");
+console.log( "Amigos de Tokyo","\n", resultado,"\n");
+ 
+ 
+const resultado2 = amigos.filter(u => u.nome.includes("W"));
+console.log("Amigos com a letra W","\n",resultado2,"\n");
 
-// Versão completa com parênteses
-let aprovados = notas.filter((nota) => {
-    return nota >= 7
-})
-
-// Todas notas
-console.log(notas)
-
-// Aprovados
-console.log(aprovados)
+const resultado3 = amigos.filter(u => u.idade.includes(16));
+console.log("Amigos com 16 anos","\n",resultado3,"\n");
